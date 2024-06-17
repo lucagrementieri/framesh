33d276229389daaf5a1d667b24d9c249b7afaf24
lucagrementieri <luca.grementieri@ens-paris-saclay.fr>
2024-06-17 08:17:00 +0200

    :arrow_up: Upgrade dependencies

--- framesh/util.py
+++ framesh/util.py
@@ -1,24 +1,9 @@
-import functools
-import time
 from pathlib import Path
-from typing import Sequence, Union
+from typing import Union
 
 import numpy as np
 import numpy.typing as npt
 import trimesh
-
-
-def timeit(method):
-    @functools.wraps(method)
-    def timed(*args, **kw):
-        ts = time.time()
-        result = method(*args, **kw)
-        te = time.time()
-
-        print("%r  %2.2f ms" % (method.__name__, (te - ts) * 1000))
-        return result
-
-    return timed
 
 
 def highlight_vertices(
