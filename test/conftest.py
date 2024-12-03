5dfc8efc0459b934b69539960ffa62448eea0e54
Luca Grementieri <luca.grementieri@ens-paris-saclay.fr>
2024-12-03 09:56:14 +0100

    :bug: Fix vertices in half sphere

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pytest
 import trimesh
@@ -7,6 +6,7 @@ import trimesh
 def half_cylinder() -> trimesh.Trimesh:
     cylinder = trimesh.creation.cylinder(radius=5.0, height=10.0)
     half_cylinder: trimesh.Trimesh = cylinder.slice_plane(np.zeros(3), np.array([1.0, 0.0, 0.0]))
+    half_cylinder.merge_vertices()
     half_cylinder = half_cylinder.subdivide().subdivide().subdivide()
     half_cylinder.export("half_cylinder.ply")
     return half_cylinder
@@ -22,6 +22,7 @@ def half_cylinder_right() -> tuple[str, trimesh.Trimesh, int]:
 def half_sphere() -> trimesh.Trimesh:
     sphere = trimesh.creation.icosphere(radius=5, subdivision=2)
     half_sphere: trimesh.Trimesh = sphere.slice_plane(np.zeros(3), np.array([0.0, 0.0, 1.0]))
+    half_sphere.merge_vertices()
     half_sphere.export("half_sphere.ply")
     return half_sphere
 
